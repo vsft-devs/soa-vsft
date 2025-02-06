268ef9cabbc4cd9feff0bb66fe81dcb7abe51fd1
aknehra <aknehra2003@gmail.com>
2025-02-06 16:55:19 +0530

    Added Data Center Service

---
+++ src/app/pages/services-data_center/services-data_center.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-data_center',
+  templateUrl: './services-data_center.component.html',
+  styleUrl: './services-data_center.component.scss'
+})
+export class ServicesDataCenterComponent {
+
+}
