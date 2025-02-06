c0bcdec32b395b1a1ec1e318e08da1f0933a347e
aknehra <aknehra2003@gmail.com>
2025-02-06 16:54:19 +0530

    Added Salesforce Service

---
+++ src/app/pages/services-salesforce-list/services-salesforce-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-salesforce-list',
+  templateUrl: './services-salesforce-list.component.html',
+  styleUrl: './services-salesforce-list.component.scss'
+})
+export class ServicesSalesforceListComponent {
+
+}
